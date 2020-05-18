d2bf6de158d618e8417879767bc9c3f24b5bfe3c
Andrew Schmitt <Andrew.Schmitt@loreal.com>
2020-05-18 03:18:14 -0400

    Lesson 37

--- complex-react-app/app/Main.js
+++ complex-react-app/app/Main.js
@@ -14,7 +14,8 @@ import Home from "./components/Home"
 import CreatePost from "./components/CreatePost"
 import ViewSinglePost from "./components/ViewSinglePost"
 import FlashMessages from "./components/FlashMessages"
-import ExampleContext from "./ExampleContext"
+import StateContext from "./StateContext"
+import DispatchContext from "./DispatchContext"
 
 function Main() {
   const initialState = {
@@ -35,38 +36,33 @@ function Main() {
 
   const [state, dispatch] = useReducer(ourReducer, initialState)
 
-  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")))
-  const [flashMessages, setFlashMessages] = useState([])
-
-  function addFlashMessage(msg) {
-    setFlashMessages(prev => prev.concat(msg))
-  }
-
   return (
-    <ExampleContext.Provider value={{addFlashMessage, setLoggedIn}}>
-      <BrowserRouter>
-        <FlashMessages messages={flashMessages} />
-        <Header loggedIn={loggedIn} />
-        <Switch>
-          <Route path="/" exact>
-            {loggedIn ? <Home /> : <HomeGuest />}
-          </Route>
-          <Route path="/about-us">
-            <About/>
-          </Route>
-          <Route path="/terms">
-            <Terms/>
-          </Route>
-          <Route path="/post/:id">
-            <ViewSinglePost />
-          </Route>
-          <Route path="/create-post">
-            <CreatePost />
-          </Route>
-        </Switch>
-        <Footer/>
-      </BrowserRouter>
-    </ExampleContext.Provider>
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <BrowserRouter>
+          <FlashMessages messages={state.flashMessages} />
+          <Header />
+          <Switch>
+            <Route path="/" exact>
+              {state.loggedIn ? <Home /> : <HomeGuest />}
+            </Route>
+            <Route path="/about-us">
+              <About/>
+            </Route>
+            <Route path="/terms">
+              <Terms/>
+            </Route>
+            <Route path="/post/:id">
+              <ViewSinglePost />
+            </Route>
+            <Route path="/create-post">
+              <CreatePost />
+            </Route>
+          </Switch>
+          <Footer/>
+        </BrowserRouter>
+      </DispatchContext.Provider>
+    </StateContext.Provider>
   )
 }
 
